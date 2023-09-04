b222bdfe3bfabbefd0f76437eddc7207c8adf0c3
Janek111271 <46629633+Janek111271@users.noreply.github.com>
2023-09-04 11:30:09 +0200

    Change views, add some unittests, final version updated to Gcloud

--- app/bouncer/views.py
+++ app/bouncer/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from google.cloud import ndb
+from django.shortcuts import render, redirect
+from django.http import Http404
+from bouncer.models import Redirect
 
-# Create your views here.
 
 def landing(request):
-    return render(request, 'bouncer/index.html')
+    redirects = Redirect.query().fetch()
+
+    return render(request, 'bouncer/index.html', {'redirects': redirects})
+
+
+def handle_redirect(request, slug):
+    redirect_entity = ndb.Key(Redirect, slug).get()
+    if not redirect_entity:
+        raise Http404('Not found')
+
+    return redirect(redirect_entity.destination_url, permanent=True)
+
